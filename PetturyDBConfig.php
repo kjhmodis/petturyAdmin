ea8af8acada7278b194bc6a606c63bc638752450
zero <kjhmodis@hanmail.net>
2020-03-26 01:27:17 +0900

    Update PetturyDBConfig.php

--- PetturyDBConfig.php
+++ PetturyDBConfig.php
@@ -3,7 +3,7 @@
     //define
 	// define (DB_USER, "root");
 	define (DB_USER, "kjhmodis");
-	define (DB_PASSWORD, "next0205");
+	define (DB_PASSWORD, "12341234");
 	define (DB_DATABASE, "pettury");
 	// define (DB_HOST, "localhost");
 	define (DB_HOST, "zero.ch3jllns7bfa.us-east-1.rds.amazonaws.com:3306");
